5370e6aed415efbd5a799332527481cf6b752619
Viktor Shpachuk <torogao@protonmail.com>
2025-01-29 16:15:41 +0200

    feat: add custom 0.1.8 twitter client

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,8 +1,8 @@
 import { AutoClientInterface } from "@elizaos/client-auto";
 import { DiscordClientInterface } from "@elizaos/client-discord";
 import { TelegramClientInterface } from "@elizaos/client-telegram";
-import { TwitterClientInterface } from "@elizaos/client-twitter";
 import { Character, IAgentRuntime } from "@elizaos/core";
+import { TwitterClientInterface } from "./client-twitter/src/index.ts";
 
 export async function initializeClients(
   character: Character,
